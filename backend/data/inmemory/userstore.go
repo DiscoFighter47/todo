e675a3188701188ed289d92f3add2a97e6ac70fc
Zahid Al Tair <mdzahidh119@gmail.com>
2019-08-20 15:55:56 +0600

    fixed backend inmemory user store concurrency support

--- backend/data/inmemory/userstore.go
+++ backend/data/inmemory/userstore.go
@@ -1,35 +1,37 @@
 package inmemory
 
 import (
+	"sync"
+
 	"github.com/DiscoFighter47/todo/backend/data"
 	"github.com/DiscoFighter47/todo/backend/model"
 )
 
 // UserStore ..
 type UserStore struct {
-	repo map[string]*model.User
+	repo sync.Map
 }
 
 // NewUserStore ...
 func NewUserStore() *UserStore {
 	return &UserStore{
-		repo: map[string]*model.User{},
+		repo: sync.Map{},
 	}
 }
 
 // AddUser ...
 func (store *UserStore) AddUser(user *model.User) error {
-	if _, ok := store.repo[user.ID]; ok {
+	if _, ok := store.repo.Load(user.ID); ok {
 		return data.ErrUserAlreadyExists
 	}
-	store.repo[user.ID] = user
+	store.repo.Store(user.ID, user)
 	return nil
 }
 
 // GetUser ...
 func (store *UserStore) GetUser(id string) (*model.User, error) {
-	if user, found := store.repo[id]; found {
-		return user, nil
+	if user, found := store.repo.Load(id); found {
+		return user.(*model.User), nil
 	}
 	return nil, data.ErrUserNotFound
 }
